2e9ba506e9fb0d4888e847733ebd0df10c141adc
anto-battaglia <email15032019@email.it>
2019-04-06 17:30:12 +0200

    while per la griglia infinita

--- Esercizi/Ordinamento4-Anto/main.cpp
+++ Esercizi/Ordinamento4-Anto/main.cpp
@@ -14,7 +14,10 @@ int main()
 
     for(int i=0;i<N;i++)
     {
+        if(i!=2)
         mat[i][0]=1;
+        else
+        mat[i][N-1]=2;
     }
 
     for(int i=0;i<N;i++)//stampa griglia iniziale
@@ -28,54 +31,70 @@ int main()
 
 
     int temp,m2=0;
-    //for(int i=0;i<N;i++)
-    //{
+    while(1)
+    {
         for(int m=0;m<N;m++)
         {
             for(int x=0;x<N;x++)
             {
-
-                m2=m+1;
-                if(m2<(N))//Ordinamento tipo bubble sort, m2<N-1 perchè deve fermarsi al penultimo ciclo del fot m2
+                if(x!=2)
                 {
 
-                    //Con questo indichiamo una posizione successiva ad m
-                    temp=mat[x][m];
-                    mat[x][m]=mat[x][m2];
-                    mat[x][m2]=temp;
+
+                    m2=m+1;
+                    if(m2<(N))//Ordinamento tipo bubble sort, m2<N-1 perchè deve fermarsi al penultimo ciclo del fot m2
+                    {
+
+                        //Con questo indichiamo una posizione successiva ad m
+                        temp=mat[x][m];
+                        mat[x][m]=mat[x][m2];
+                        mat[x][m2]=temp;
+
+                    }
+
+                    else// Spostiamo i valori a fine colonna, a inizio colonna
+                    {
+
+                        temp=mat[x][m];
+                        mat[x][m]=mat[x][m-(N-1)];
+                        mat[x][m-(N-1)]=temp;
+                        m2=0;
+                    }
+
+
+
 
                 }
 
-                else// Spostiamo i valori a fine colonna, a inizio colonna
-                {
-
-                    temp=mat[x][m];
-                    mat[x][m]=mat[x][m-(N-1)];
-                    mat[x][m-(N-1)]=temp;
-                    m2=0;
-                }
 
 
+            }
 
-
+                system("cls");
                 printf("\n");
                 printf("\n");
                 for(int i=0;i<N;i++)
                 {
                     for(int m=0;m<N;m++)
                     {
-                        printf(" %d ",mat[i][m]);
+                        //printf(" %d ",mat[i][m]);
+                        if(mat[i][m]==1)
+                            printf(" # ");
+                        else if(mat[i][m]==0)
+                            printf("   ");
+                        else
+                            printf(" @ ");
                     }
                     printf("\n");
                 }
-                getchar();
+                //getchar();
+                Sleep(500);
 
 
-            }
 
 
         }
-    //}
+    }
 
 
 
